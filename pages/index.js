d88fddb8fc72d0260fd7ba657bb4d4a2f4ce6947
PaulineDuarte <paulineduarte@live.com>
2023-09-02 01:04:16 -0300

    Inicial commit

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import { getCookie} from "cookies-next"
+import { useEffect } from "react"
+import { verifica } from "./services/user"
+
+
+export default function Home() {
+  return (
+    <div>
+      Pagina segura - Perfil do usuário
+    </div>
+  )
+}
+
+
+export const getServeSideProps = async ({req,res}) => {
+    try{
+      const token = getCookie ("authorization",{req,res})
+       if (!token) throw new Error ("Token Inválido") 
+
+       verifica(token)
+      return {
+        props: {}
+      }
+    } catch (err) {
+
+      return {
+        redirect: {
+          permanent : false,
+          destination:"/login"
+        },
+        props: {}
+      }
+    }
+} 
